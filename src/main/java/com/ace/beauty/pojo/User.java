79c3421c3040abcc3ca134ed5f40022b80ffd6b4
Ace.L <ace_liang@126.com>
2017-09-14 00:50:33 +0800

    new file User

---
+++ src/main/java/com/ace/beauty/pojo/User.java
@@ -0,0 +1,37 @@
+package com.ace.beauty.pojo;
+
+public class User {
+	private Integer userId;
+	private String username;
+	private Integer age;
+	private Integer gender;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", age=" + age + ", gender=" + gender + "]";
+	}
+}
