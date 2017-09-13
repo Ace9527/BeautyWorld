e2f53bb76f2e0a92c434ef86794c2dcacfff0431
Ace.L <ace_liang@126.com>
2017-09-14 00:41:11 +0800

    new project.

---
+++ src/main/java/com/ace/beauty/BeautyWorldApplication.java
@@ -0,0 +1,12 @@
+package com.ace.beauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyWorldApplication.class, args);
+	}
+}
